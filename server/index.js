9793f741fdf49a85294441049d4a1489f9c04d68
lschpmn <luis@lmc-productions.com>
2016-02-15 16:06:44 -0600

    Server stable enough for development of front-end

--- server/index.js
+++ server/index.js
@@ -3,8 +3,11 @@
 const search = require('./middleware/search');
 const express = require('express');
 const app = express();
-app.listen(5000);
 
-app.use('/api/stories', search.router);
+app.use('/api/stories', search);
 
 app.use(express.static(__dirname + '/../public/'));
+
+app.listen(5000);
+
+console.log('Server started on port 5000');
