f0b82c3390fe2c3625b2bba9b27a7988ed82553e
lschpmn <luis@lmc-productions.com>
2016-02-15 16:12:39 -0600

    Angular compiled successfully

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const gulp = require('gulp');
+const gulp = /**@type {gulp}*/ require('gulp');
+const webpack = require('webpack-stream');
+const jade = require('gulp-jade');
 const child_process = require('child_process');
 let child;
 
@@ -8,8 +10,10 @@ gulp.task('default', ['server', 'client']);
 
 gulp.task('watch', ['server-watch', 'client-watch']);
 
+//server
+
 gulp.task('server', () => {
-  if(child) child.kill();
+  if(child) child.kill('SIGKILL');
 
   child = child_process.fork('./server/index.js');
 });
@@ -18,8 +22,27 @@ gulp.task('server-watch', () => {
   return gulp.watch(['./server/*.js', './server/**/*.js'], ['server']);
 });
 
-gulp.task('client', () => {
+//client
 
+gulp.task('client', ['compile-js', 'compile-jade']);
+
+gulp.task('client-watch', ['compile-js', 'compile-jade'], () => {
+  gulp.watch('./client/**/*.js', ['compile-js']);
+  gulp.watch('./client/**/*.jade', ['compile-jade']);
 });
 
-gulp.task('client-watch');
+//js
+
+gulp.task('compile-js', () => {
+  return gulp.src('./client/js/index.js')
+    .pipe(webpack({output: {filename: 'require.js'}}))
+    .pipe(gulp.dest('./public'));
+});
+
+//jade
+
+gulp.task('compile-jade', () => {
+  return gulp.src(['./client/jade/*.jade', './client/jade/**/*.jade'])
+    .pipe(jade())
+    .pipe(gulp.dest('./public'));
+});
