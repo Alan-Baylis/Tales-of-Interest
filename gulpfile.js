681d36addfe750daa7aad26459fa15b27d2354d6
lschpmn <luis@lmc-productions.com>
2016-02-16 00:54:42 -0600

    Renamed gulp tasks, added support for legacy js injection.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 const gulp = /**@type {gulp}*/ require('gulp');
 const jade = require('gulp-jade');
 const browserify = require('gulp-browserify');
+const inject = require('gulp-inject');
 const child_process = require('child_process');
 let child;
 
@@ -19,29 +20,38 @@ gulp.task('server', () => {
 });
 
 gulp.task('server-watch', ['server'], () => {
-  return gulp.watch(['./server/*.js', './server/**/*.js'], ['server']);
+  return gulp.watch(['./server/**/*.js'], ['server']);
 });
 
 //client
 
-gulp.task('client', ['compile-js', 'compile-jade']);
+gulp.task('client', ['js', 'jade']);
 
 gulp.task('client-watch', ['client'], () => {
-  gulp.watch('./client/**/*.js', ['compile-js']);
-  gulp.watch('./client/**/*.jade', ['compile-jade']);
+  gulp.watch('./client/**/*.js', ['js']);
+  gulp.watch('./client/**/*.jade', ['jade']);
 });
 
 //js
 
-gulp.task('compile-js', () => {
+gulp.task('js', ['legacy-js'], () => {
   return gulp.src('./client/js/app.js')
     .pipe(browserify())
     .pipe(gulp.dest('./public'));
 });
 
+gulp.task('legacy-js', () => {
+  const stream = gulp.src('./client/plugins/**/*.js')
+    .pipe(gulp.dest('./public'));
+
+  return gulp.src('./public/index.html')
+    .pipe(inject(stream, {ignorePath: '/public'}))
+    .pipe(gulp.dest('./public'));
+});
+
 //jade
 
-gulp.task('compile-jade', () => {
+gulp.task('jade', () => {
   return gulp.src(['./client/jade/**/*.jade'])
     .pipe(jade({pretty: true}))
     .pipe(gulp.dest('./public'));
