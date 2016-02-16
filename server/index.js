1d47b7e5196fc439d97efb6ea770f97c6ff2b8a8
lschpmn <luis@lmc-productions.com>
2016-02-15 18:20:57 -0600

    404 redirects to index

--- server/index.js
+++ server/index.js
@@ -8,6 +8,11 @@ app.use('/api/stories', search);
 
 app.use(express.static(__dirname + '/../public/'));
 
+app.use((req, res) => {
+  console.log(req.originalUrl + ': lead nowhere. Redirecting to index');
+  res.sendFile('public/index.html', {root: __dirname + '/../'});
+});
+
 app.listen(5000);
 
 console.log('Server started on port 5000');
