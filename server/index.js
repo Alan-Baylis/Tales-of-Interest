58673d76fde98f34f73004830b32d7a7c755bdde
lschpmn <luis@lmc-productions.com>
2016-05-02 23:17:39 -0500

    Added cors

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@
 
 const search = require('./middleware/search');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const PORT = process.env.tal_PORT || 5001;
+
+app.use(cors());
 
 app.use('/', (req,res, next) => {
   console.log(`${new Date().toLocaleString()} - ${req.method} - ${req.url}`);
