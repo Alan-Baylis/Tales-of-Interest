e499d6db4927e6d0595cf7e962f0384831f72653
lschpmn <luis@lmc-productions.com>
2016-05-02 23:43:01 -0500

    SearchComponent now makes a call to the server and displays results in a list

--- client/components/search/SearchComponent.js
+++ client/components/search/SearchComponent.js
@@ -1,5 +1,39 @@
 'use strict';
 
+const bluebird = /**@type {Promise}*/ require('bluebird');
+const axios = require('axios');
+const searchRequest = (term) => axios.post('http://me:5001/api/search', {term: term});
+
+bluebird.config({
+  cancellation: true
+});
+
 module.exports = {
-  template: require('jade!./SearchTemplate.jade')()
+  template: require('jade!./SearchTemplate.jade')(),
+  
+  data: () => ({
+    search: '',
+    searchResults: []
+  }),
+  
+  computed: {
+    showList: function() {
+      return this.searchResults.length !== 0;
+    }
+  },
+  
+  watch: {
+    search: function(val) {
+      if(this.$data.promise) this.$data.promise.cancel();
+  
+      if(val === '') return this.$data.searchResults = [];
+      
+      this.$data.promise = bluebird.delay(500)
+        .then(() => searchRequest(val))
+        .then(res => {
+          this.$data.searchResults = res.data;
+        })
+        .catch(err => console.error(err));
+    }
+  }
 };
