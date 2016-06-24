1bd286132c91cabf56041f8d93f06200d0e19644
lschpmn <luis@lmc-productions.com>
2016-06-24 15:25:44 -0500

    Log time of search

--- server/middleware/search.js
+++ server/middleware/search.js
@@ -7,9 +7,11 @@ const StorySearcher = require('../lib/StorySearcher');
 const storySearcher = new StorySearcher(conf.DB_CONN);
 
 router.post('/', (req, res) => {
+  const now = Date.now();
+  
   storySearcher.query(req.body.query, 0)
     .then(ids => {
-      console.log(`For query: ${req.body.query}, got ${ids.length} ids`);
+      console.log(`For query: ${req.body.query}, got ${ids.length} ids, time taken: ${Date.now() - now}ms`);
   
       res.send(ids);
     });
