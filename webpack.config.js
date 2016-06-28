973e7b39de6e25a502b8dd19b45ac55d0969b0cb
lschpmn <luis@lmc-productions.com>
2016-06-28 15:38:38 -0500

    Added webpack-dev-server config to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,27 @@ const path = require('path');
 
 module.exports = {
   context: path.join(__dirname, 'client'),
+  
   entry: './app.js',
+  
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'require.js'
   },
+  
   module: {
     loaders: [
       {test: /\.css$/, loader: 'style!css'},
       {test: /\.scss$/, loader: `style!css!sass`},
-      {test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.eot$|\.woff2$/, loader: 'file?name=[name].[ext]'},
+      {test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.eot$|\.woff2$/, loader: 'file?name=/[name].[ext]'},
       {test: /index.jade/, loader: 'file?name=[name].html!jade-html?pretty'}
     ]
+  },
+  
+  devServer: {
+    inline: true,
+    host: '0.0.0.0',
+    port: 5000,
+    historyApiFallback: true
   }
 };
