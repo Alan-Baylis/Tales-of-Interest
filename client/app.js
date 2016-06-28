3097cedc6bddd58bc70c6e65f3cf5b5757c77461
lschpmn <luis@lmc-productions.com>
2016-06-28 15:39:11 -0500

    Added story view

--- client/app.js
+++ client/app.js
@@ -8,10 +8,17 @@ require('./index.scss');
 
 const vue = require('vue');
 const vueRouter = require('vue-router');
-const SearchComponent = require('./components/search/SearchComponent.js');
+const SearchComponent = require('./components/search/SearchComponent');
+const StoryView = require('./views/story-view/StoryView');
 
 vue.use(vueRouter);
-const router = new vueRouter();
+const router = new vueRouter({history: true});
+
+router.map({
+  '/story/:id': {
+    component: StoryView
+  } 
+});
 
 router.start({
   components: {
---
+++ client/views/story-view/StoryView.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+  template: require('jade!./StoryTemplate.jade')()
+};
