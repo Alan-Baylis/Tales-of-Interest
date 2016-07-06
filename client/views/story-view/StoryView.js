73ed33a3815433d05e846ff94b16bfcfdb6e36e2
lschpmn <luis@lmc-productions.com>
2016-07-06 14:45:49 -0500

    Story view working

--- client/views/story-view/StoryView.js
+++ client/views/story-view/StoryView.js
@@ -1,5 +1,44 @@
 'use strict';
 
+const axios = require('axios');
+
 module.exports = {
-  template: require('jade!./StoryTemplate.jade')()
+  template: require('jade!./StoryTemplate.jade')(),
+  
+  data: () => ({
+    text: ''
+  }),
+  
+  route: {
+    data: function() {
+      const id = this.$route.params.id;
+  
+      axios.get('http://me:5001/api/story/' + id)
+        .then(res => {
+          const textArr = /**@type {String[]}*/ res.data.split('');
+          let formattedText = '';
+          let lastChar = '';
+          
+          //gets rid of line breaks but preserves paragraph breaks
+          for(let char of textArr) {
+            if(char === '\n') {
+              if(lastChar === '\n') {
+                formattedText += '\n\n';
+                continue;
+              }
+              
+              lastChar = char;
+              formattedText += ' ';
+              continue;
+            }
+            
+            lastChar = char;
+            formattedText += char;
+          }
+          
+          this.text = formattedText;
+        })
+        .catch(err => console.error(err));
+    }
+  }
 };
